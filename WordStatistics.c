712317c116e8d960f51b7816e6fcd6b3d8d82143
Ralph <19510622+RalXYZ@users.noreply.github.com>
2019-12-12 11:30:42 +0800

    增加了统计字符数的功能

--- WordStatistics.c
+++ WordStatistics.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
-bool isLetter (char input); // to judge whether a character is a letter
+bool isLetter (char input); // 判断一个字符是否为英文字母
 int main () {
     FILE *fp;
-    if (!(fp = fopen("input.txt", "r"))) { // change the file name into yours
+    if (!(fp = fopen("input.txt", "r"))) { // 请将文件名改为你想判断的文件的文件名
         printf("File open error!\n");
         exit(0);
     }
-
-    int sum = 0; // max_Amount: 2147483647
+////////////////////////////////////////////////////////////////////////////////
+    int sum = 0; // 最大单词数: 2147483647
     char currentChar;
-    bool currentCharJudge = false, previousCharJudge = false; // a necessary initialization, to guarantee the performance of the loop below
-    do {
+    bool currentCharJudge = false, previousCharJudge = false; // 一个必要的初始化，它与以下的do-while的第一次循环的正确执行有关
         currentChar = fgetc(fp);
         if (isLetter(currentChar))
             currentCharJudge = true;
@@ -21,9 +20,15 @@ int main () {
             ++sum;
         previousCharJudge = currentCharJudge;
     } while (currentChar != EOF);
-
-    printf("The amount of word in this paragraph is: %d", sum);
-
+    printf("The amount of word in this paragraph is: %d\n", sum);
+////////////////////////////////////////////////////////////////////////////////
+    fseek(fp, 0L, SEEK_SET);
+    long fileStart = ftell(fp);
+    fseek(fp, 0L, SEEK_END);
+    long fileEnd = ftell(fp);
+    printf("%ld", fileEnd - fileStart);
+    printf("The amount of character in this paragraph is: %ld\n", fileEnd - fileStart);
+////////////////////////////////////////////////////////////////////////////////
     if (fclose (fp)) {
         printf("File close error!\n");
         exit(0);
