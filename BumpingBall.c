37df1b55a18215b764731138f737a69643b2e54c
Ralph <19510622+RalXYZ@users.noreply.github.com>
2019-12-14 14:27:41 +0800

    增加必要的注释

--- BumpingBall.c
+++ BumpingBall.c
@@ -5,6 +5,7 @@
 #include <windows.h>
 
 int main() {
+	/*用户输入*/
 	int numberOfRow, numberOfColumn;
 	printf("Please type in the number of row (<100): ");
 	scanf("%d", &numberOfRow);
@@ -12,18 +13,20 @@ int main() {
 	scanf("%d", &numberOfColumn);
 	system("cls");
 
+	/*一些必要的初始化*/
 	srand((unsigned)time(NULL));
 	int currentRow = rand() % numberOfRow, currentColumn = rand() % numberOfColumn;
 	int judgeRow = 1, judgeColumn = 1;
 	int time = 0;
-
 	char core[100][100] = { 0 };
 	char screen[104][102] = { 0 };
 	for (int i = 0; i < numberOfRow; i++)
 		for (int j = 0; j < numberOfColumn; j++)
 			core[i][j] = ' ';
 
+	/*小球的每一次移动即为下面的一次循环*/
 	while (true) {
+		/*小球换向*/
 		if (currentRow == numberOfRow - 1)
 			judgeRow = -1;
 		else if (currentRow == 0)
@@ -32,10 +35,15 @@ int main() {
 			judgeColumn = -1;
 		else if (currentColumn == 0)
 			judgeColumn = 1;
+
+		/*小球移位并清除上一个位置的输出*/
 		core[currentRow][currentColumn] = ' ';
 		core[currentRow += judgeRow][currentColumn += judgeColumn] = '*';
 
+		/*将要输出的数据统一放至screen[][]*/
 		for (int i = 0; i < numberOfRow + 2; i++) {
+			/*存入首行和末行的边框，其他情况下存入首列和末列的边框以及棋盘*/
+			/*注意，为了输出美观，考虑到一个字符所占像素，故在core的每一个元素后加一个空格*/
 			if (i == 0 || i == numberOfRow + 1) {
 				for (int j = 0; j < numberOfColumn + 2; j++) {
 					screen[i][2 * j] = '*';
@@ -56,10 +64,12 @@ int main() {
 			}
 		}
 
+		/*统一输出*/
 		for (int i = 0; i < numberOfRow + 2; i++)
 			printf("%s", screen[i]);
 		printf("time: %d\n", time);
 
+		/*计数，暂停与清屏*/
 		time++;
 		Sleep(70);
 		system("cls");
