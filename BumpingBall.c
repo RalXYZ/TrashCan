b738abe715cca7a90c80bef5327940012bf6848b
Ralph <19510622+RalXYZ@users.noreply.github.com>
2019-12-16 21:41:23 +0800

    更本质的物理，更精简的代码
    将'#'墙移到core数组内，从而使识别当前“地形”成为可能。  
    把反弹定义为向量取反，而非+1 -1，这是更本质的物理学原理。  
    对于墙外角的情况进行了特判，使弹弹球的行为适应任何情况，为下一步允许用户自定义地形打下基础。  
    重写输出模块，由于以上架构，使输出被大大简化。 
    将暂停时间写在宏定义中。

--- BumpingBall.c
+++ BumpingBall.c
@@ -5,9 +5,11 @@
 #include <stdbool.h>
 #include <windows.h>
 #define LENGTH 100
+#define WAIT 50
 
 int main() {
 	/*用户输入*/
+	//TODO 用户自定义地形
 	int numberOfRow, numberOfColumn;
 	printf("Please type in the number of row (<%d): ", LENGTH);
 	scanf("%d", &numberOfRow);
@@ -17,26 +19,38 @@ int main() {
 
 	/*一些必要的初始化*/
 	srand((unsigned)time(NULL));
-	int currentRow = rand() % numberOfRow, currentColumn = rand() % numberOfColumn;
+	int currentRow = rand() % numberOfRow + 1, currentColumn = rand() % numberOfColumn + 1; //在这里+1是因为棋盘中0号元素必为'#'
 	int vectorRow = pow(-1, rand() % 2), vectorColumn = pow(-1, rand() % 2);
 	int time = 0;
-	char core[LENGTH][LENGTH] = { 0 };
-	char screen[2 * (LENGTH + 1)][2 * (LENGTH + 2)] = { 0 };
-	for (int i = 0; i < numberOfRow; i++)
-		for (int j = 0; j < numberOfColumn; j++)
-			core[i][j] = ' ';
+	char core[LENGTH + 2][LENGTH + 2] = { 0 };
+	char screen[2 * (LENGTH + 2) + 1][2 * (LENGTH + 2) + 1] = { 0 };
 
-	/*小球的每一次移动即为下面的一次循环*/
+	/*棋盘预处理*/
+	for (int i = 0; i < numberOfRow + 2; i++) {
+		if (i == 0 || i == numberOfRow + 1) //在首行和末行全置入'#'
+			for (int j = 0; j < numberOfColumn + 2; j++)
+				core[i][j] = '#';
+		else //在其它行的开始与末尾置入'#'
+			for (int j = 0; j < numberOfColumn + 2; j++)
+				if (j == 0 || j == numberOfColumn + 1)
+					core[i][j] = '#';
+				else
+					core[i][j] = ' ';
+	}
+		
+	/*小球的每一次移动即为下面的每一次循环*/
 	while (true) {
 		/*小球换向*/
-		if (currentRow == numberOfRow - 1)
-			vectorRow = -1;
-		else if (currentRow == 0)
-			vectorRow = 1;
-		if (currentColumn == numberOfColumn - 1)
-			vectorColumn = -1;
-		else if (currentColumn == 0)
-			vectorColumn = 1;
+		if (core[currentRow + vectorRow][currentColumn] == '#')
+			vectorRow = -vectorRow;
+		if (core[currentRow][currentColumn + vectorColumn] == '#')
+			vectorColumn = -vectorColumn;
+		if (core[currentRow + vectorRow][currentColumn] != '#' 
+			&& core[currentRow][currentColumn + vectorColumn] != '#' 
+			&& core[currentRow + vectorRow][currentColumn + vectorColumn] == '#') {
+			vectorRow = -vectorRow;
+			vectorColumn = -vectorColumn;
+		}
 
 		/*小球移位并清除上一个位置的输出*/
 		core[currentRow][currentColumn] = ' ';
@@ -44,26 +58,11 @@ int main() {
 
 		/*将要输出的数据统一放至screen[][]*/
 		for (int i = 0; i < numberOfRow + 2; i++) {
-			/*存入首行和末行的边框，其他情况下存入首列和末列的边框以及棋盘*/
-			/*注意，为了输出美观，考虑到一个字符所占像素，故在core的每一个元素后加一个空格*/
-			if (i == 0 || i == numberOfRow + 1) {
-				for (int j = 0; j < numberOfColumn + 2; j++) {
-					screen[i][2 * j] = '#';
-					screen[i][2 * j + 1] = ' ';
-				}
-				screen[i][2 * (numberOfColumn + 1) + 1] = '\n';
-				continue;
+			for (int j = 0; j < numberOfColumn + 2; j++) {
+				screen[i][2 * j] = core[i][j];
+				screen[i][2 * j + 1] = ' '; //注意，为了输出美观，考虑到字符所占像素，故在每一个元素后加一个空格
 			}
-			else {
-				screen[i][0] = '#';
-				screen[i][1] = ' ';
-				for (int j = 1; j < numberOfColumn + 2; j++) {
-					screen[i][2 * j] = core[i - 1][j - 1];
-					screen[i][2 * j + 1] = ' ';
-				}
-				screen[i][2 * (numberOfColumn + 1)] = '#';
-				screen[i][2 * (numberOfColumn + 1) + 1] = '\n';
-			}
+			screen[i][2 * (numberOfColumn + 1) + 1] = '\n';
 		}
 
 		/*统一输出*/
@@ -73,7 +72,7 @@ int main() {
 
 		/*计数，暂停与清屏*/
 		time++;
-		Sleep(50);
+		Sleep(WAIT);
 		system("cls");
 	}
 	return 0;
